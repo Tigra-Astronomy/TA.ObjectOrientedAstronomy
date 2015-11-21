6292ee1f2bde3f4225aa5a291f033f2cf1d254c3
Tim Long <Tim@tigranetworks.co.uk>
2015-11-21 16:51:47 +0000

    A last minute refactoring broke the InternalsVisibleTo attributes; fixed.

--- TA.ObjectOrientedAstronomy/Properties/AssemblyInfo.cs
+++ TA.ObjectOrientedAstronomy/Properties/AssemblyInfo.cs
@@ -36,4 +36,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 // For unit test
-[assembly:InternalsVisibleTo("TA.Orbits.Specifications")]
+[assembly:InternalsVisibleTo("TA.ObjectOrientedAstronomy.Specifications")]
